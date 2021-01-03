af003c57972150c323b4737a0141bab328420f97
Nathan Verzemnieks <njvrzm@gmail.com>
2021-01-02 21:33:30 -0800

    Now with ellipses

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     for i in 0..=128 {
         let fi = (i as f32)/128.0;
         x = (x+37.7*fi+w/2.0+ t*t)%w - w/2.0 + (10*(i%10-5)) as f32;
-        y = (y+67.7*fi+h/2.0 + t*t)%h - h/2.0;
+        y = (y+67.7+h/2.0 + t*t)%h - h/2.0;
         r = r+199*((255.0*x) as u8);
         g = g+37;
         b = b+100;
@@ -59,15 +59,15 @@ fn view(app: &App, _model: &Model, frame: Frame) {
             .xy(rec.xy())
             .w_h(rec.w()/2.0, rec.h())
             .x_y(x, y)
-            .z_degrees(360.0*(sint+x/w))
+            .z_turns(sint+x/w)
             .color(rgba8(255-r, 255-g, 255-b, a));
-
-        canvas.rect()
+        canvas.ellipse()
             .xy(rec.xy())
-            .wh(rec.wh())
+            .w_h(rec.w()*1.5, rec.h()/3.0)
             .x_y(x, y)
-            .z_degrees(360.0 * (sint+x/w))
+            .z_turns(sint+x/w)
             .color(rgba8(r, g, b, a));
+
     }
 
     // put everything on the frame
